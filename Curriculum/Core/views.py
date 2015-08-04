ebbc02d542b206edb5f5f4f6fb5e7e514b5e2b2a
Skelther <mguillau@student.42.fr>
2015-08-05 01:12:02 +0200

    Portfolio done
    portfolio parses all projects entries in database, some css needs to be
    done though

--- Curriculum/Core/views.py
+++ Curriculum/Core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 # Create your views here.
 from Presentation.models import Profile
+from Portfolio.models import Project
 
 
 def index(request):
     profiles = Profile.objects.all()
-    return render(request, 'Core/index.html', {'profiles': profiles})
+    projects = Project.objects.all()
+    return render(request, 'Core/index.html', {'profiles': profiles, 'projects': projects})
