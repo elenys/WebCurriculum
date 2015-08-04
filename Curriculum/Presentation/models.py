8828c8369da50192e032461d4d535605e12ef722
elenys <bmartins@student.42.fr>
2015-08-04 19:52:24 +0200

    Adding Profile Model
    Adding Profile Model with some information about the user( lastname /
    firstaname / picture / ...)

--- Curriculum/Presentation/models.py
+++ Curriculum/Presentation/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    picture = models.ImageField(upload_to='image')
+    intro = models.CharField(max_length=400)
+    dob = models.DateField('date of birth')
+
+    def __str__(self):
+        return self.first_name
+
+    def age(self):
+        today = date.today()
+        years = int((today - self.dob).days / 365.2425)
+        return years
