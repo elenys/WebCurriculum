3b82c8322fad11175bc77f44152d99f20c598b6b
elenys <bmartins@student.42.fr>
2015-08-05 18:46:48 +0200

    Del Old files

--- git/urls.py
+++ git/urls.py
@@ -1,4 +1,4 @@
-"""Curriculum URL Configuration
+"""git URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-from Core import views
+from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', views.index, name="index"),
+    url(r'^admin/', include(admin.site.urls)),
 ]
--- git/wsgi.py
+++ git/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Curriculum project.
+WSGI config for git project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Curriculum.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "git.settings")
 
 application = get_wsgi_application()
