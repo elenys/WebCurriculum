4f2968392ad18e7b8589260eb8ff54bf8e244ed4
elenys <bmartins@student.42.fr>
2015-08-06 15:06:20 +0200

    Creation Class to get api informations

--- Presentation/views.py
+++ Presentation/views.py
@@ -4,4 +4,4 @@ from Presentation.models import Profile
 
 def presentation(request):
     profiles = Profile.objects.all()
-    return render(request, 'Presentation/presentation.html', {'profiles': profiles})
+    return {'profiles': profiles}
